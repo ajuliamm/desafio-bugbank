a7e726a464807c178a2fbea92f89392fc9808e68
Angelina Julia de França Mendonça de Melo <angelina.melo@dbserver.com.br>
2024-08-23 20:50:45 -0300

    :construction: Page Factory implementation in RegisternPage

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -1,57 +1,88 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class RegisterPage extends PageObject{
+
+    @FindBy(xpath = "//div[@class='card__register']//input[@name='email']")
+    private WebElement emailTextField; 
+
+    @FindBy(xpath = "//div[@class='card__register']//input[@name='name']")
+    private WebElement nameTextField; 
+
+    @FindBy(xpath = "//div[@class='card__register']//input[@name='password']")
+    private WebElement passwordTextField; 
+
+    @FindBy(xpath = "//div[@class='card__register']//input[@name='passwordConfirmation']")
+    private WebElement passwordConfirmationTextField; 
+    
+    @FindBy(xpath = "//button[text()='Registrar']")
+    private WebElement registerButton; 
+    
+    @FindBy(xpath = "//div[@class='card__register']//p[text()='É campo obrigatório']")
+    private WebElement inputWarning; 
+    
+    @FindBy(xpath = "//label[@id='toggleAddBalance']//..")
+    private WebElement addBalanceToggle; 
+
+    @FindBy(xpath = "//div[@class='card__register']//button[text()='Cadastrar']")
+    private WebElement submitFormRegisterButton; 
+
+    @FindBy(id = "modalText")
+    private WebElement modalText; 
+
+    @FindBy(id = "btnCloseModal")
+    private WebElement closeModalButton; 
+
 
     public RegisterPage(WebDriver driver) {
         super(driver);
     }
 
     public WebElement getRegisterButton() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Registrar']"))); 
+        return wait.until(ExpectedConditions.visibilityOf(registerButton)); 
     }
 
     public WebElement getEmailField() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card__register']//input[@name='email']"))); 
+        return wait.until(ExpectedConditions.visibilityOf(emailTextField)); 
     }
 
     public WebElement getNameField() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card__register']//input[@name='name']"))); 
+        return wait.until(ExpectedConditions.visibilityOf(nameTextField)); 
     }
 
     public WebElement getPasswordField() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card__register']//input[@name='password']"))); 
+        return wait.until(ExpectedConditions.visibilityOf(passwordTextField)); 
     }
 
     public WebElement getPasswordConfirmationField() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card__register']//input[@name='passwordConfirmation']"))); 
+        return wait.until(ExpectedConditions.visibilityOf(passwordConfirmationTextField)); 
     }
 
     public WebElement getAddBalanceToggle() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[@id='toggleAddBalance']//.."))); 
+        return wait.until(ExpectedConditions.visibilityOf(addBalanceToggle)); 
     }
 
     public WebElement getSubmitFormRegisterButton() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card__register']//button[text()='Cadastrar']"))); 
-
-    }
-
-    public WebElement getModalText() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("modalText"))); 
-
-    }
-
-    public WebElement getCloseModalButton() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btnCloseModal"))); 
+        return wait.until(ExpectedConditions.visibilityOf(submitFormRegisterButton)); 
 
     }
 
     public WebElement getInputWarning() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card__register']//p[text()='É campo obrigatório']"))); 
+        return wait.until(ExpectedConditions.visibilityOf(inputWarning)); 
+
+    }
+
+    public WebElement getModalText() {
+        return wait.until(ExpectedConditions.visibilityOf(modalText)); 
+
+    }
+
+    public WebElement getCloseModalButton() {
+        return wait.until(ExpectedConditions.visibilityOf(closeModalButton)); 
 
     }
 
