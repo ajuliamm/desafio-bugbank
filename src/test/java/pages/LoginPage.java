96bf646171a804604130da2d52a4457939d827df
Angelina Julia de França Mendonça de Melo <angelina.melo@dbserver.com.br>
2024-08-23 20:34:12 -0300

    :construction: Page Factory implementation in LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,6 +4,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends PageObject {
@@ -12,30 +13,50 @@ public class LoginPage extends PageObject {
         super(driver);
     }
 
+    @FindBy(xpath = "(//input[@name='email'])[1]")
+    private WebElement emailTextField; 
+
+    @FindBy(xpath = "(//input[@name='password'])[1]")
+    private WebElement passwordTextField; 
+    
+    @FindBy(className = "otUnI")
+    private WebElement logginButton; 
+    
+    @FindBy(xpath = "//div[@class='card__login']//p[text()='É campo obrigatório']")
+    private WebElement inputWarning; 
+    
+    @FindBy(id = "modalText")
+    private WebElement modalText; 
+
+    @FindBy(id = "btnCloseModal")
+    private WebElement closeModalButton; 
+    
+
     public WebElement getEmailTextField (){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@name='email'])[1]")));
+        return wait.until(ExpectedConditions.visibilityOf(emailTextField));
+
     }
     
     public WebElement getPasswordTextField (){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@name='password'])[1]")));
+        return wait.until(ExpectedConditions.visibilityOf(passwordTextField));
     }
 
     public WebElement getLoginButton() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".otUnI")));
+        return wait.until(ExpectedConditions.visibilityOf(logginButton));
     }
 
     public WebElement getInputWarning() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card__login']//p[text()='É campo obrigatório']"))); 
+        return wait.until(ExpectedConditions.visibilityOf(inputWarning)); 
 
     }
 
     public WebElement getModalText() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("modalText"))); 
+        return wait.until(ExpectedConditions.visibilityOf(modalText)); 
 
     }
 
     public WebElement getCloseModalButton() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btnCloseModal"))); 
+        return wait.until(ExpectedConditions.visibilityOf(closeModalButton)); 
 
     }
 
--- src/test/java/pages/PageObject.java
+++ src/test/java/pages/PageObject.java
@@ -2,6 +2,7 @@ package pages;
 
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PageObject {
@@ -12,6 +13,7 @@ public class PageObject {
 
         public PageObject(WebDriver driver){
                 this.driver = driver; 
+                PageFactory.initElements(driver, this);
                 this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
         }
